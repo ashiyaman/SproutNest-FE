93f0a37d123707c991be32d98a244766db6cdf47
ashiyaman <ashiya1005@gmail.com>
2025-05-20 13:43:58 +0530

    fix: Fix styles for mor products section

--- src/components/HorizontalProductsLit.jsx
+++ src/components/HorizontalProductsLit.jsx
@@ -1,38 +1,52 @@
-import { useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { fetchProductById } from "./products/productSlice"
+import { fetchProductById } from "./products/productSlice";
 
-const HorizontalProductList = ({products}) => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+import './horizontalProductList.css'
 
-    const productDetailHandler = (productId) => {        
-        dispatch(fetchProductById(productId))
-        navigate(`/products/${productId}`)
-    }
+const HorizontalProductList = ({ products }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    return (        
-        <div className='d-flex flex-wrap w-100 justify-content-around' >
-            {products.map(product =>
-                <div className='mx-2' key={product._id}>
-                    <div  className='card rounded border-0' style={{cursor: 'pointer'}}>
-                    <img
-                        src={product.images[0]} 
-                        alt={product.name} 
-                        className='img-fluid border rounded'
-                        style={{height: '40vh', width: 'auto', border: '1px solid #224d43 !important'}}
-                        onClick={() => productDetailHandler(product._id)} />
-                        <div className="card-body" style={{color: '#224d43', backgroundColor: '#F5F5DC'}}>
-                            <p className="card-title fw-bold">{product.name}</p>
-                            <p><i className="bi bi-star-fill text-warning"> </i>{product.rating} | {product.reviews.length} reviews</p>
-                            <p className='fw-semibold'>₹ {product.price}</p>
-                        </div>                
-                    </div>
-                </div>
-            )}
-        </div>        
-    )
-}
+  const productDetailHandler = (productId) => {
+    dispatch(fetchProductById(productId));
+    navigate(`/products/${productId}`);
+  };
 
-export default HorizontalProductList
+  return (
+    <div className="horizontal-scroll-wrapper py-2">
+      <div className="horizontal-scroll d-flex">
+        {products.map((product) => (
+          <div className="product-card mx-2" key={product._id}>
+            <div
+              className="card rounded border-0"
+              style={{ cursor: "pointer" }}
+            >
+              <img
+                src={product.images[0]}
+                alt={product.name}
+                className="img-fluid border rounded"
+                style={{ height: "40vh", width: "auto" }}
+                onClick={() => productDetailHandler(product._id)}
+              />
+              <div
+                className="card-body"
+                style={{ color: "#224d43", backgroundColor: "#F5F5DC" }}
+              >
+                <p className="card-title fw-bold">{product.name}</p>
+                <p>
+                  <i className="bi bi-star-fill text-warning"></i>{" "}
+                  {product.rating} | {product.reviews.length} reviews
+                </p>
+                <p className="fw-semibold">₹ {product.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HorizontalProductList;
